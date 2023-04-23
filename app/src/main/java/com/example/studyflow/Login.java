e8495cfa9e4a441c411e43e093547dfd24059e79
alosadahg <hgalosada@gmail.com>
2023-04-23 13:54:45 +0800

    Modified login to require a name

--- app/src/main/java/com/example/studyflow/Login.java
+++ app/src/main/java/com/example/studyflow/Login.java
@@ -2,7 +2,12 @@ package com.example.studyflow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -10,6 +15,23 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        getSupportActionBar().hide();
+
+        Button btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        EditText txtName = (EditText) findViewById(R.id.editTextUsername);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = txtName.getText().toString();
+                if(name.equals("")) {
+                    Toast.makeText(Login.this, "Please enter a name", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+                    mainIntent.putExtra("name", name);
+                    startActivity(mainIntent);
+                }
+            }
+        });
     }
 }
