9dab1465a7b09ec42e471ab1f386706f49b6e7be
alosadahg <hgalosada@gmail.com>
2023-04-23 13:55:36 +0800

    Added dark mode for Main Activity

--- app/src/main/java/com/example/studyflow/MainActivity.java
+++ app/src/main/java/com/example/studyflow/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.studyflow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("name");
+        TextView msgWelcome = (TextView) findViewById(R.id.msgWelcome);
+
+        msgWelcome.setText("Welcome " + username + "!");
+
+        ImageView imgTheme = findViewById(R.id.imgTheme);
+        int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        if (nightModeFlags == Configuration.UI_MODE_NIGHT_NO) {
+            // Night mode is not active
+            imgTheme.setBackgroundResource(R.drawable.theme_light);
+        } else {
+            // Night mode is active
+            imgTheme.setBackgroundResource(R.drawable.theme);
+        }
+        imgTheme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+                if (nightModeFlags == Configuration.UI_MODE_NIGHT_NO) {
+                    // Night mode is not active
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else {
+                    // Night mode is active
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
+
     }
 }
