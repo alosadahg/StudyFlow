2e1d18ddeefc6fb3ed0cf0465e8f3fbea14904c8
alosadahg <hgalosada@gmail.com>
2023-05-05 22:10:59 +0800

    Added utilities to main

--- app/src/main/java/com/example/studyflow/MainActivity.java
+++ app/src/main/java/com/example/studyflow/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.studyflow;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,23 @@ public class MainActivity extends AppCompatActivity {
 
         msgWelcome.setText("Welcome " + username + "!");
 
+        CardView cardPomodoro = findViewById(R.id.crdPomodoro);
+        CardView cardTodo = findViewById(R.id.crdTodo);
+        CardView cardQuizzes = findViewById(R.id.crdQuizzes);
+        CardView cardFlashcards = findViewById(R.id.crdFlashcards);
+        CardView cardNotes = findViewById(R.id.crdNotes);
+        CardView cardStudySet = findViewById(R.id.crdStudySet);
+
+        cardPomodoro.setOnClickListener(this);
+        cardTodo.setOnClickListener(this);
+        cardQuizzes.setOnClickListener(this);
+        cardFlashcards.setOnClickListener(this);
+        cardNotes.setOnClickListener(this);
+        cardStudySet.setOnClickListener(this);
+
+
         ImageView imgTheme = findViewById(R.id.imgTheme);
+
         int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
         if (nightModeFlags == Configuration.UI_MODE_NIGHT_NO) {
             // Night mode is not active
@@ -46,5 +65,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.crdPomodoro:
+                Toast.makeText(MainActivity.this,"Pomodoro timer is clicked!",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.crdTodo:
+                Toast.makeText(MainActivity.this,"To-do list is clicked!",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.crdQuizzes:
+                Toast.makeText(MainActivity.this,"Quizzes is clicked!",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.crdFlashcards:
+                Toast.makeText(MainActivity.this,"Flashcards is clicked!",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.crdNotes:
+                Toast.makeText(MainActivity.this,"Notes is clicked!",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.crdStudySet:
+                Toast.makeText(MainActivity.this,"Study sets is clicked!",Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
