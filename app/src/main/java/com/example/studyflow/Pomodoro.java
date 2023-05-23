0956a27e6dd0c658b20020dba3065d8511d9620b
harknoel <bontilao.harknoel@gmail.com>
2023-05-23 18:36:24 +0800

    Add pomodoro timer functionality

--- app/src/main/java/com/example/studyflow/Pomodoro.java
+++ app/src/main/java/com/example/studyflow/Pomodoro.java
@@ -2,12 +2,15 @@ package com.example.studyflow;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import com.google.android.material.button.MaterialButtonToggleGroup;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,6 +21,10 @@ public class Pomodoro extends AppCompatActivity {
     private static final long POMODORO_TIME = 25 * 60 * 1000; // 25 minutes in milliseconds
     private static final long LONG_BREAK_TIME = 15 * 60 * 1000; // 15 minutes in milliseconds
     private static final long SHORT_BREAK_TIME = 5 * 60 * 1000; // 5 minutes in milliseconds
+    private Button btnFocus, btnShortBreak, btnLongBreak;
+    private MaterialButtonToggleGroup toggleButton;
+    private Button btnPomodoroStart;
+    private int selectedButtonId;
     @Override
     public void onBackPressed() {
         super.onBackPressed();
@@ -33,6 +40,69 @@ public class Pomodoro extends AppCompatActivity {
         ImageView imgTheme = findViewById(R.id.imgTheme);
         ImageView catTheme = findViewById(R.id.catTheme);
         ImageView ic_close = findViewById(R.id.ic_close);
+
+        btnFocus = findViewById(R.id.btnPomodoroFocus);
+        btnShortBreak = findViewById(R.id.btnPomodoroShortBreak);
+        btnLongBreak = findViewById(R.id.btnPomodoroLongBreak);
+        btnPomodoroStart = findViewById(R.id.btnPomodoroStart);
+        toggleButton = findViewById(R.id.toggleButton);
+
+        selectedButtonId = -1;
+        toggleButton.addOnButtonCheckedListener(new MaterialButtonToggleGroup.OnButtonCheckedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public void onButtonChecked(MaterialButtonToggleGroup group, int checkedId, boolean isChecked) {
+                // Check if the button is checked
+                if (isChecked) {
+                    // Handle the button selection
+                    selectedButtonId = checkedId;
+                    if(isTimerRunning) {
+                        stopTimer();
+                    }
+                    switch (selectedButtonId) {
+                        case R.id.btnPomodoroFocus:
+                            // Perform action for the "Focus" button
+                            updateTimerText(POMODORO_TIME);
+                            break;
+                        case R.id.btnPomodoroLongBreak:
+                            // Perform action for the "Long" button
+                            updateTimerText(LONG_BREAK_TIME);
+                            break;
+                        case R.id.btnPomodoroShortBreak:
+                            updateTimerText(SHORT_BREAK_TIME);
+                            // Perform action for the "Short" button
+                            break;
+                    }
+                }
+            }
+        });
+        imgTheme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopTimer();
+            }
+        });
+
+        btnPomodoroStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selectedButtonId != -1) {
+                    if (!isTimerRunning) {
+                        switch (selectedButtonId) {
+                            case R.id.btnPomodoroFocus:
+                                startPomodoroTimer();
+                                break;
+                            case R.id.btnPomodoroLongBreak:
+                                startLongBreakTimer();
+                                break;
+                            case R.id.btnPomodoroShortBreak:
+                                startShortBreakTimer();
+                                break;
+                        }
+                    }
+                }
+            }
+        });
 
         txtPomodoroTimer = findViewById(R.id.txtPomodoroTimer);
         txtPomodoroTimer.setOnClickListener(new View.OnClickListener() {
@@ -101,6 +171,7 @@ public class Pomodoro extends AppCompatActivity {
     }
 
     private void startLongBreakTimer() {
+
         timer = new CountDownTimer(LONG_BREAK_TIME, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -138,10 +209,10 @@ public class Pomodoro extends AppCompatActivity {
         isTimerRunning = true;
     }
 
+    @SuppressLint("NonConstantResourceId")
     private void stopTimer() {
         timer.cancel();
         isTimerRunning = false;
-        updateTimerText(POMODORO_TIME);
     }
 
     private void updateTimerText(long millisUntilFinished) {
@@ -150,5 +221,11 @@ public class Pomodoro extends AppCompatActivity {
 
         String timeText = String.format("%02d:%02d", minutes, seconds);
         txtPomodoroTimer.setText(timeText);
+    }
+
+    private void toggleButtonStates(boolean focusEnabled, boolean shortBreakEnabled, boolean longBreakEnabled) {
+        btnFocus.setEnabled(focusEnabled);
+        btnShortBreak.setEnabled(shortBreakEnabled);
+        btnLongBreak.setEnabled(longBreakEnabled);
     }
 }
