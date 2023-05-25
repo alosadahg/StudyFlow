1855902a96a544e16d9b8925fe9e53454b96b8cc
harknoel <bontilao.harknoel@gmail.com>
2023-05-25 10:18:20 +0800

    Refactor quiz

--- app/src/main/java/com/example/studyflow/Quizzes.java
+++ app/src/main/java/com/example/studyflow/Quizzes.java
@@ -84,6 +84,14 @@ public class Quizzes extends AppCompatActivity{
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
                 switch(menuItem.getItemId()) {
+                    case R.id.nav_reminder:
+                        Intent reminder = new Intent(getApplicationContext(),Reminder.class);
+                        startActivity(reminder);
+                        return true;
+                    case R.id.nav_countdownTimer:
+                        Intent timer = new Intent(getApplicationContext(), CountdownTimer.class);
+                        startActivity(timer);
+                        return true;
                     case R.id.nav_logout:
                         SharedPreferences sharedPreferences = getSharedPreferences("current_user", Context.MODE_PRIVATE);
                         SharedPreferences.Editor editor = sharedPreferences.edit();
